f01e207e94717172a4c9bfd201044f4b45f6b94b
monstergelo <bimawansatrianto@gmail.com>
2022-05-25 15:18:18 +0700

    stratigraphic page

--- pages/oil_and_gas/stratigraphic_services.tsx
+++ pages/oil_and_gas/stratigraphic_services.tsx
@@ -1,19 +1,20 @@
-import BasicLayout from "module/BasicLayout";
 import StratigraphicServices from "module/StratigraphicServices";
 import { PageType } from "module/types";
 import { STRATIGRAPGIC_SERVICES as PAGE } from "pageConstant/oil_and_gas";
-import imgBackground from "assets/bg-energyevolution.png";
 import { getBreadcrumb } from "pageConstant";
+import OilAndGasLayout from "module/BasicLayout/OilAndGasLayout";
 
 const StratigraphicServicesPage: PageType = StratigraphicServices;
-StratigraphicServicesPage.layout = BasicLayout;
+StratigraphicServicesPage.layout = OilAndGasLayout;
 StratigraphicServicesPage.title = PAGE.title;
 
 export const getStaticProps = async () => {
+  const imgBackground = "/assets/bg-equipment.png";
+
   return {
     props: {
-      text: "CORE ANALYSIS",
-      backgroundImage: imgBackground?.src,
+      text: "Stratigraphic Services",
+      backgroundImage: imgBackground,
       breadcrumb: getBreadcrumb(PAGE.title),
     },
   };
