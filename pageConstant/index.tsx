797f235edbd6a342b8a15f700d5decb3877ef998
monstergelo <bimawansatrianto@gmail.com>
2022-05-31 13:42:09 +0700

    remove placeholder

--- pageConstant/index.tsx
+++ pageConstant/index.tsx
@@ -3,13 +3,8 @@ import { generateBreadcrumb } from "reusables/utilities";
 import { default as OIL_AND_GAS } from "./oil_and_gas";
 
 export { default as HOME } from "./home";
-export { default as ABOUT } from "./about";
-export { default as CONTACT } from "./contact-us";
-export { default as EVENT } from "./event";
-export { default as LAB } from "./lab";
-export { default as SEARCH } from "./search";
-export { default as NEWS } from "./news";
 export { default as OIL_AND_GAS } from "./oil_and_gas";
+
 const thisBreadcrumb = [
   {
     title: "Home",
@@ -19,7 +14,7 @@ const thisBreadcrumb = [
 ];
 
 const breadcrumb: BreadcrumbMapProps = {
-  "Oil and Gas": thisBreadcrumb,
+  Home: thisBreadcrumb,
   ...generateBreadcrumb(OIL_AND_GAS.breadcrumb, thisBreadcrumb),
 };
 
