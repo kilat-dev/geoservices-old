6affc353631c49bfe62c239386022d26b392dfef
monstergelo <bimawansatrianto@gmail.com>
2022-06-21 16:23:39 +0700

    header hover

--- pages/sandbox.tsx
+++ pages/sandbox.tsx
@@ -6,15 +6,49 @@ const Sandbox = () => {
   return (
     <Box>
       <Text>
-      Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed mollis velit sit amet vestibulum rhoncus. Nulla iaculis orci dui, et accumsan lacus consequat fringilla. Quisque sodales lorem sit amet ornare maximus. Donec quis volutpat lorem. Nam volutpat ut lacus et laoreet. Praesent ut odio imperdiet, luctus ante vel, sagittis lacus. Nullam at ligula eget lorem sollicitudin auctor. Phasellus efficitur tellus tellus, in euismod mauris commodo vel. Mauris ullamcorper consequat velit, eu dignissim lacus iaculis at. Vivamus suscipit orci metus, a efficitur lorem ullamcorper vel. Integer congue nulla at odio vulputate viverra. Proin eu lacinia orci. Mauris non egestas enim, id tempor purus. Integer ullamcorper blandit massa non sagittis. Nullam rutrum lorem sit amet sapien imperdiet, sed efficitur lacus tempor.Sed aliquam, quam nec feugiat efficitur, mauris elit scelerisque leo, ac luctus mauris justo eu turpis. Ut vehicula, ipsum id condimentum iaculis, eros nunc molestie ligula, id sagittis velit augue eget lorem. In placerat posuere ligula. Aenean nec urna sapien. Aenean in nulla a sem condimentum euismod non quis tellus. Fusce justo nibh, egestas eget molestie at, lobortis et ex. Nullam metus odio, blandit a ultricies sit amet, posuere eget diam. Etiam auctor tellus risus, eu sodales ligula sodales eget. Nunc malesuada sem nec nisl consequat, eget sagittis lectus commodo. Etiam nec arcu quis elit auctor pulvinar nec sit amet justo. Etiam blandit sapien vitae justo efficitur, in dictum justo mattis. Nullam leo eros, finibus a semper non, cursus in purus.
-
-Curabitur sapien nisi, venenatis ut hendrerit non, pharetra vel lectus. Duis mi ex, imperdiet ut sollicitudin in, consequat non elit. Vestibulum felis urna, consectetur id consequat eu, iaculis non tellus. Etiam placerat ligula quis tellus euismod, quis sagittis lectus convallis. Ut molestie, eros non tempus consectetur, ante justo vulputate mauris, nec convallis augue mi in velit. Sed iaculis ex sit amet lobortis euismod. Praesent nec lacus in neque venenatis pellentesque. Phasellus tincidunt ante vitae eros pellentesque, in laoreet leo accumsan. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae;
-
-Sed quis condimentum diam. Phasellus aliquam maximus diam, et lacinia orci consectetur eu. Proin pretium bibendum sagittis. In et aliquam neque. Fusce commodo magna sit amet cursus efficitur. Curabitur quis vestibulum ante. Proin quis ultricies nibh.
-
-Donec consequat, eros eu egestas luctus, tortor tortor scelerisque mi, in vulputate neque diam a mi. Morbi quam enim, lobortis eu lorem quis, aliquet malesuada mi. Aliquam sem sapien, mollis nec interdum eu, placerat eget orci. Phasellus egestas nisi lorem, sed fringilla risus viverra non. Sed viverra odio lacinia felis egestas, ac finibus arcu sagittis. Sed maximus imperdiet massa, vitae vestibulum nisi. Mauris commodo sit amet mi non tincidunt. Nulla pretium bibendum justo ut placerat.
-
-
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed mollis
+        velit sit amet vestibulum rhoncus. Nulla iaculis orci dui, et accumsan
+        lacus consequat fringilla. Quisque sodales lorem sit amet ornare
+        maximus. Donec quis volutpat lorem. Nam volutpat ut lacus et laoreet.
+        Praesent ut odio imperdiet, luctus ante vel, sagittis lacus. Nullam at
+        ligula eget lorem sollicitudin auctor. Phasellus efficitur tellus
+        tellus, in euismod mauris commodo vel. Mauris ullamcorper consequat
+        velit, eu dignissim lacus iaculis at. Vivamus suscipit orci metus, a
+        efficitur lorem ullamcorper vel. Integer congue nulla at odio vulputate
+        viverra. Proin eu lacinia orci. Mauris non egestas enim, id tempor
+        purus. Integer ullamcorper blandit massa non sagittis. Nullam rutrum
+        lorem sit amet sapien imperdiet, sed efficitur lacus tempor.Sed aliquam,
+        quam nec feugiat efficitur, mauris elit scelerisque leo, ac luctus
+        mauris justo eu turpis. Ut vehicula, ipsum id condimentum iaculis, eros
+        nunc molestie ligula, id sagittis velit augue eget lorem. In placerat
+        posuere ligula. Aenean nec urna sapien. Aenean in nulla a sem
+        condimentum euismod non quis tellus. Fusce justo nibh, egestas eget
+        molestie at, lobortis et ex. Nullam metus odio, blandit a ultricies sit
+        amet, posuere eget diam. Etiam auctor tellus risus, eu sodales ligula
+        sodales eget. Nunc malesuada sem nec nisl consequat, eget sagittis
+        lectus commodo. Etiam nec arcu quis elit auctor pulvinar nec sit amet
+        justo. Etiam blandit sapien vitae justo efficitur, in dictum justo
+        mattis. Nullam leo eros, finibus a semper non, cursus in purus.
+        Curabitur sapien nisi, venenatis ut hendrerit non, pharetra vel lectus.
+        Duis mi ex, imperdiet ut sollicitudin in, consequat non elit. Vestibulum
+        felis urna, consectetur id consequat eu, iaculis non tellus. Etiam
+        placerat ligula quis tellus euismod, quis sagittis lectus convallis. Ut
+        molestie, eros non tempus consectetur, ante justo vulputate mauris, nec
+        convallis augue mi in velit. Sed iaculis ex sit amet lobortis euismod.
+        Praesent nec lacus in neque venenatis pellentesque. Phasellus tincidunt
+        ante vitae eros pellentesque, in laoreet leo accumsan. Vestibulum ante
+        ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae;
+        Sed quis condimentum diam. Phasellus aliquam maximus diam, et lacinia
+        orci consectetur eu. Proin pretium bibendum sagittis. In et aliquam
+        neque. Fusce commodo magna sit amet cursus efficitur. Curabitur quis
+        vestibulum ante. Proin quis ultricies nibh. Donec consequat, eros eu
+        egestas luctus, tortor tortor scelerisque mi, in vulputate neque diam a
+        mi. Morbi quam enim, lobortis eu lorem quis, aliquet malesuada mi.
+        Aliquam sem sapien, mollis nec interdum eu, placerat eget orci.
+        Phasellus egestas nisi lorem, sed fringilla risus viverra non. Sed
+        viverra odio lacinia felis egestas, ac finibus arcu sagittis. Sed
+        maximus imperdiet massa, vitae vestibulum nisi. Mauris commodo sit amet
+        mi non tincidunt. Nulla pretium bibendum justo ut placerat.
       </Text>
     </Box>
   );
