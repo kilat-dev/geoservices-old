370c645d03387a926979527378a5584d18e8958c
Sebastianus Bara Primananda <primanandas@DNID1561L01.local>
2022-12-23 13:54:28 +0700

    fix linter issue

--- pages/oil_and_gas/geolab.tsx
+++ pages/oil_and_gas/geolab.tsx
@@ -1,24 +1,22 @@
 import Geolab from "module/Geolab";
 import { PageType } from "module/types";
-import { GEOLAB as PAGE } from "pageConstant/oil_and_gas";
-import { getBreadcrumb } from "pageConstant";
 import OilAndGasLayout from "module/BasicLayout/OilAndGasLayout";
 
 const GeolabPage: PageType = Geolab;
 GeolabPage.layout = OilAndGasLayout;
-GeolabPage.title = 'GEOLAB';
+GeolabPage.title = "GEOLAB";
 
 export const getStaticProps = async () => {
-    const imgBackground = "/assets/geolab.png";
+  const imgBackground = "/assets/geolab.png";
 
-    return {
-        props: {
-            text: "Geolab",
-            backgroundImage: imgBackground,
-            // breadcrumb: getBreadcrumb('GEOLAB'),
-        },
-        revalidate: 10,
-    };
+  return {
+    props: {
+      text: "Geolab",
+      backgroundImage: imgBackground,
+      // breadcrumb: getBreadcrumb('GEOLAB'),
+    },
+    revalidate: 10,
+  };
 };
 
 export default GeolabPage;
